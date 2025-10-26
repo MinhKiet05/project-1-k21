78261bb26e534ee16547bd19b5c6dca90eda7c76
MinhKiet05 <tranvanbetu1965@gmail.com>
2025-10-26 16:13:14 +0700

    xong header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,6 +4,11 @@ export function Header() {
   return (
     <header className="header">
       <h1>NoTungPhoCo Marketplace</h1>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/products">Products</a>
+        <a href="/about">About</a>
+      </nav>
     </header>
   );
 }
