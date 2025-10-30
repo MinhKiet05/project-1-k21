041afa89c4ebb7e6b22ace296a43a1d8f067f59e
MinhKiet05 <tranvanbetu1965@gmail.com>
2025-10-30 23:24:05 +0700

    may be done head/footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -16,16 +16,16 @@ function Footer() {
 
         {/* Liên kết nhanh */}
         <div className="footer-links">
-          <h3>Liên kết nhanh</h3>
+          <h3>Các liên kết</h3>
           <ul>
             <li><a href="#"><u>Trang chủ</u></a></li>
-            <li><a href="#"><u>Về chúng tôi</u></a></li>
+            <li><a href="#"><u>Giới thiệu</u></a></li>
           </ul>
         </div>
 
         {/* Liên hệ */}
         <div className="footer-contact">
-          <h3>Liên hệ giúp đỡ</h3>
+          <h3>Liên hệ</h3>
           <ul>
             <li><b>Email:</b> <a href="mailto:notungphoco@gmail.com">notungphoco@gmail.com</a></li>
             <li><b>Số điện thoại:</b> <a href="tel:0336748385">0336748385</a></li>
