e170ce5e25c1ec767b9ab7dad29ecdd066d7cc53
MinhKiet05 <tranvanbetu1965@gmail.com>
2025-10-30 22:02:21 +0700

    tétttt

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import "./Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import HeaderLogo from '../../assets/logo.png';
+import HeaderLogo from '../../assets/headerLogo.png';
 import {
   faHouse,
   faPen,
