25488c175771116214b0fd0773b62e296ef4dc4a
MinhKiet05 <tranvanbetu1965@gmail.com>
2025-11-02 00:29:33 +0700

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Header from './components/header/temp.jsx'
+import Header from './components/header/Header.jsx'
 import Footer from './components/footer/Footer.jsx'  
 import HomePage from './pages/homePage/HomePage.jsx'
 import UploadPost from './pages/uploadPost/UploadPost.jsx'
